366a64a84e6c233d4fe9dc2a6fe1fca7d0eb0977
robbyawaldi <robbyawaldi@gmail.com>
2019-07-26 05:38:47 +0700

    fix

---
+++ src/model/Login.java
@@ -0,0 +1,40 @@
+package model;
+
+import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
+import org.sql2o.Connection;
+
+/**
+ *
+ * @author Tohar
+ */
+public class Login extends RecursiveTreeObject<Login>{
+    private String username;
+    private String password;
+    
+    public static Login getLogin(String username) {
+        try (Connection connection = DB.DB.sql2o.open()){
+            final String query = "SELECT * FROM login WHERE `username`=:username";
+            return connection.createQuery(query).addParameter("username",username).executeAndFetchFirst(Login.class);
+        }
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+    
+    
+}
