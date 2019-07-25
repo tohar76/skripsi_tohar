0ae9f8b152318d41911acc6123b8317ffbb3d0d0
robbyawaldi <robbyawaldi@gmail.com>
2019-07-26 05:45:42 +0700

    fix Login

--- src/model/Login.java
+++ src/model/Login.java
@@ -1,14 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model;
 
-/**
- *
- * @author asus
- */
-public class Login {
+import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
+import org.sql2o.Connection;
+
+
+public class Login extends RecursiveTreeObject<Login>{
+    private String username;
+    private String password;
+    
+    public static Login getLogin(String username) {
+        try (Connection connection = DB.DB.sql2o.open()){
+            final String query = "SELECT * FROM login WHERE `username`=:username";
+            return connection.createQuery(query).addParameter("username",username).executeAndFetchFirst(Login.class);
+        }
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+    
     
 }
